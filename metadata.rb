5ffcbef95b5c698d155431ff873696227f0b02ac
Jonathan Hartman <j@p4nt5.com>
2016-03-21 10:03:07 -0700

    Update mac-app-store dep

--- metadata.rb
+++ metadata.rb
@@ -8,6 +8,6 @@ description      'Installs/Configures the Twitter app'
 long_description 'Installs/Configures the Twitter app'
 version          '0.1.1'
 
-depends          'mac-app-store', '~> 0.1'
+depends          'mac-app-store', '~> 1.0'
 
 supports         'mac_os_x'
